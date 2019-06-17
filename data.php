0723f3251dea656763516d04ce458e0055235ad4
CarolaTeramo <carolateramo@gmail.com>
2019-06-17 16:43:24 +0200

    fase 2

--- data.php
+++ data.php
@@ -10,7 +10,7 @@
   // genera_codici_giocatori($n)
   // e nel for al posto di 3 mettere $n
 
-  function genera_codici_giocatori() {
+  function genera_giocatore() {
 
     $codice_giocatore = "";
 
@@ -20,21 +20,21 @@
     for ($i=0; $i < 3; $i++) {
       $tre_lettere = rand(0, $lunghezza_alfabeto - 1);
       $codice_giocatore .= $lettere[$tre_lettere];
-      var_dump($codice_giocatore);
-      echo '<br>';
-      echo $codice_giocatore;
+      //var_dump($codice_giocatore);
+      //echo '<br>';
+      //echo $codice_giocatore;
     }
 
     for ($i=0; $i < 3; $i++) {
       $tre_numeri = rand(1,9);
       $codice_giocatore .= $tre_numeri;
-      var_dump($codice_giocatore);
-      echo '<br>';
-      echo $codice_giocatore;
+      //var_dump($codice_giocatore);
+      //echo '<br>';
+      //echo $codice_giocatore;
     }
 
     // ho ottenuto il codice ora lo inserisco nell'array
-
+    $codici_giocatori[] = $codice_giocatore;
 
     // genero numero di punti, rimbalzi e Falli
     $punti = rand(0, 40);
@@ -43,27 +43,43 @@
 
     // genero le percentuali per i tiri da 2 e 3 punti
     $perc_2 = (rand(0,1000) / 10);
-    echo '<br>';
+    //echo '<br>';
 
-    echo '<prev>';
-    var_dump($perc_2);
-    echo '</prev>';
+    // echo '<prev>';
+    // var_dump($perc_2);
+    // echo '</prev>';
     $perc_3 = (100 - $perc_2);
 
     $giocatore = [
-      [
-        'codice' => $codice_giocatore,
-        'punti' => $punti,
-        'rimbalzi' => $rimbalzi,
-        'falli' => $falli,
-        'perc_2' => $perc_2,
-        'perc_3' => $perc_3,
-
-      ],
+      'codice' => $codice_giocatore,
+      'punti' => $punti,
+      'rimbalzi' => $rimbalzi,
+      'falli' => $falli,
+      'perc_2' => $perc_2,
+      'perc_3' => $perc_3,
     ];
-    echo '<br>';
-    var_dump($giocatore);
+
+    // echo '<br>';
+    // var_dump($giocatore);
+
+    return $giocatore;
   };// fine funzione genera codici
 
-  echo genera_codici_giocatori();
+
+  $giocatori = [];
+
+  for ($i=0; $i <100 ; $i++) {
+    $giocatore = genera_giocatore();
+    $prova = array_push($giocatori,$giocatore);
+    //var_dump($giocatori);
+  }
+
+  foreach ($giocatori as $key => $singolo_giocatore) {
+    echo $singolo_giocatore['codice']. '<br>';
+    // foreach ($singolo_giocatore as $key => $value) {
+    //   echo $value;
+    // }
+  }
+
+
 ?>
